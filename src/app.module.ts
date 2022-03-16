ba49746cc6ca8f1596c264243a61add2d2f40a11
cecep januardi <cecepjanuardi31@gmail.com>
2022-03-16 23:04:01 +0700

    fix db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostModule } from './post/post.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PostModule } from './post/post.module';
 
 @Module({
   imports: [
     PostModule,
-    MongooseModule.forRoot('mongodb://pilput:pilput31@localhost'),
+    MongooseModule.forRoot('mongodb://pilput:pilput31@localhost:27017', {
+      dbName: 'nestapi',
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
